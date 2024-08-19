87017664f66c4dd320dd89a112fa48516a3d3303
Thiago <thiagobossun06@gmail.com>
2024-08-19 10:26:44 -0300

    salvando alteracoes

--- pesquisaOperacional/lib/pesquisaOperacional_web/live/po_home.ex
+++ pesquisaOperacional/lib/pesquisaOperacional_web/live/po_home.ex
@@ -7,14 +7,25 @@ defmodule PesquisaOperacionalWeb.PoHomeLive do
 
   #inicio da construcao de UI, ainda nao funcional
   def render(assigns) do
-    ~L"""
-    <div>
-      <h1>Elixir Simplex</h1>
+    ~H"""
+    <h1 class="text-center font-bold">Elixir Simplex</h1>
+    <div class="text-center">
       <p>Método:</p>
+      <.form for={:new_po} let={f} phx-submit="po_form_submit">
+          <%= text_input f, :message %>
+      </.form>
       <p>Número de variáveis de decisão:</p>
+      <input type="text" />
       <p>Número de restrições:</p>
-      <button>Continuar</button>
+      <input type="text" />
+      <br>
+      <button class="bg-cyan-400">Confirmar</button>
     </div>
     """
+  end
+
+  def handle_event("po_form_submit", %{"po_form" => %{"message" => message}}, socket) do
+
+    {:noreply, socket}
   end
 end
